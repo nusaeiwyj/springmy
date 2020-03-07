60a9619ca9bcd04fb8904b6bd50b06d365c8b35d
nusaeiwyj <nusaei@qq.com>
2020-03-07 18:37:04 +0800

    init

---
+++ src/main/java/com/nusaeiwyj/springmy/SpringmyApplication.java
@@ -0,0 +1,13 @@
+package com.nusaeiwyj.springmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmyApplication.class, args);
+    }
+
+}
