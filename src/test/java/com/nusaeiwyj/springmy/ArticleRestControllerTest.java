016a9b6f8bf54976835d9cbe0a7b3ae2e3f6b614
nusaeiwyj <nusaei@qq.com>
2020-03-07 23:16:42 +0800

    Mock简单应用

---
+++ src/test/java/com/nusaeiwyj/springmy/ArticleRestControllerTest.java
@@ -0,0 +1,64 @@
+package com.nusaeiwyj.springmy;
+
+import com.nusaeiwyj.springmy.controller.ArticleRestController;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import javax.annotation.Resource;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@Slf4j
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+public class ArticleRestControllerTest {
+
+    //mock对象
+    @Resource
+    private MockMvc mockMvc;
+
+    //mock对象初始化
+//    @BeforeEach
+//    public void setUp() {
+//        //ArticleRestController aa = new ArticleRestController();
+//        mockMvc = MockMvcBuilders.standaloneSetup(new ArticleRestController()).build();
+//    }
+
+    //测试方法
+    @Test
+    public void saveArticle() throws Exception {
+
+        String article = "{\n" +
+                "    \"id\": 1,\n" +
+                "    \"author\": \"zimug\",\n" +
+                "    \"title\": \"手摸手教你开发spring boot\",\n" +
+                "    \"content\": \"c\",\n" +
+                "    \"createTime\": \"2017-07-16 05:23:34\",\n" +
+                "    \"reader\":[{\"name\":\"zimug\",\"age\":18},{\"name\":\"kobe\",\"age\":37}]\n" +
+                "}";
+        MvcResult result = mockMvc.perform(
+                MockMvcRequestBuilders.request(HttpMethod.POST, "/rest/article")
+                .contentType("application/json").content(article))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.author").value("zimug"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.reader[0].age").value(18))
+                .andDo(print())
+                .andReturn();
+
+        log.info(result.getResponse().getContentAsString());
+
+    }
+}
